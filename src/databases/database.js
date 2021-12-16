6cc06272fd29e08680d105d8140be6a99f7ad8b7
Eugene-Bykovsky <bykovsky.ssau@gmail.com>
2021-12-17 00:38:35 +0400

    fix path of article image

--- src/databases/database.js
+++ src/databases/database.js
@@ -2,14 +2,14 @@ export let articlesData = [
     {
         id: 1,
         title: 'Возможно про порадки на Рождество стоит подумать уже сейчас',
-        imageSrc: '../public/images1',
+        imageSrc: '/images/article1.jpeg',
         description: 'В этом году, в связи с глобальной нехваткой товаров, вызванной не Санта-Клаусом, а срывом цепочки поставок покупателям рекомендуется начинать присматриваться к подаркам намного раньше запланированного срока',
         text: 'Глобальные логистические проблемы накапливаются по мере приближения к концу года, как и ожидалось. В связи с этим некоторые советуют тем, кто собирался покупать рождественские подарки, делать это прямо сейчас, поскольку в декабре может образоваться дефицит всего и вся. Конечно, это еще раз подтверждает, что глобальная логистическая цепочка очень уязвима и оказалась именно сейчас под сильнейшим давлением.\nПо всему миру, прежде всего в Европе (а больше всего в Великобритании) хронически не хватает работников необходимых для функционирования логистики, в основном водителей. Каждый день ожидания провоцирует каскад проблем, которые наслаиваются на предыдущие, и в итоге складывается ситуация, когда на логистических путях образуются заторы.\nДля покупателей это неминуемо влечет повышение цен. Уже сейчас мы видим, как стремительно растут цены на некоторые товары. Например, на графические карты для компьютеров. Они не только подорожали, и зачастую их просто невозможно приобрести (если только не обратиться к изобретательным посредникам, которые немилосердно задирают цены). В данном конкретном случае проблема двоякая, поскольку карты стали менее доступны как из-за логистических проблем, так и потому, что используются при так называемом майнинге криптовалют.'
     },
     {
         id: 2,
         title: 'Будущее уже близко...Что ждет нас там',
-        imageSrc: '../../public/images2',
+        imageSrc: '/images/article2.jpeg',
         description: 'Мы, конечно, знаем, что мир будущего будет населен роботами, Марс обязательно будет колонизирован и однажды искусственный интеллект попытается уничтожить человечество. Но мы вспомнили о вещах второго плана, которые могли ускользнуть от вашего внимания.',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -17,7 +17,7 @@ export let articlesData = [
     {
         id: 3,
         title: 'Зеленая энергетика набирает обороты в мире. Почему?' ,
-        imageSrc: '../../public/images3',
+        imageSrc: '/images/article3.jpeg',
         description: 'Зеленая энергетика набирает обороты как на Западе, так и на Востоке. Все больше возрастает доля возобновляемых источников энергии, которые постепенно теснят традиционные источники энергии – нефть, газ и уголь. Некоторые страны планируют к 2035–2050 годам полностью отказаться от ископаемых энергоносителей, перейдя на безуглеродное обеспечение.',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
