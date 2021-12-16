018b21a22ad12b7d9ffa4cedfa7db5c1fe332be6
Eugene-Bykovsky <bykovsky.ssau@gmail.com>
2021-12-17 00:22:44 +0400

    add database articles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import MainRouter from './components/MainRouter/MainRouter'
+import {articlesData} from "./databases/database";
+
+export const AppContext = createContext(null)
 
 function App() {
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        setArticles(articlesData)
+    }, [])
+
     return (
-        <div className={'App'}>
-            <Header />
-            {Main(<MainRouter />)}
-        </div>
+        <AppContext.Provider value={{articles, setArticles}}>
+            <div className={'App'}>
+                <Header />
+                {Main(<MainRouter />)}
+            </div>
+        </AppContext.Provider>
     )
 }
 
--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from 'react'
+import Article from "../../components/Article/Article";
+import { AppContext } from '../../App'
+
 
 const IndexPage = () => {
+    const {articles} = useContext(AppContext)
+
     return (
-        <h1>Главная страница</h1>
+        <div>
+            <h1>Статьи</h1>
+            {articles.map((article) => {
+                return <Article key={article.id} article={article}/>
+            })}
+        </div>
     )
 }
 
